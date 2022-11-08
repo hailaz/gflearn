8e815c6c4c9215eaa9b23861b9c14429ceb5f848
hailaz <739476267@qq.com>
2022-11-08 15:26:17 +0800

    fix: mylog

--- os/0-log/main.go
+++ os/0-log/main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"os"
 
 	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gctx"
 	"github.com/gogf/gf/v2/os/glog"
-	"github.com/gogf/gf/v2/text/gstr"
+	"github.com/gogf/gf/v2/util/gconv"
 )
 
 // JsonOutputsForLogger is for JSON marshaling in sequence.
@@ -19,17 +18,30 @@ type JsonOutputsForLogger struct {
 
 // LoggingJsonHandler is a example handler for logging JSON format content.
 var LoggingJsonHandler glog.Handler = func(ctx context.Context, in *glog.HandlerInput) {
-	jsonForLogger := JsonOutputsForLogger{
-		Time:    in.TimeFormat,
-		Level:   gstr.Trim(in.LevelFormat, "[]"),
-		Content: gstr.Trim(in.Content),
+	str := ""
+	// TODO: something
+	config := in.Logger.GetConfig()
+	if in.TraceId != "" {
+		str += "{" + in.TraceId + "}"
+	} else {
+		str += "{}"
 	}
-	jsonBytes, err := json.Marshal(jsonForLogger)
-	if err != nil {
-		_, _ = os.Stderr.WriteString(err.Error())
-		return
+	if len(config.CtxKeys) > 0 {
+		for _, ctxKey := range config.CtxKeys {
+			var ctxValue interface{}
+			if ctxValue = ctx.Value(ctxKey); ctxValue == nil {
+				ctxValue = ctx.Value(gctx.StrKey(gconv.String(ctxKey)))
+			}
+			if ctxValue != nil {
+				str += "{" + gconv.String(ctxValue) + "}"
+			} else {
+
+				str += "{}"
+			}
+		}
 	}
-	in.Buffer.Write(jsonBytes)
+	in.Buffer.WriteString(str)
+	in.Buffer.WriteString(in.Content)
 	in.Buffer.WriteString("\n")
 	in.Next(ctx)
 }
@@ -37,6 +49,9 @@ var LoggingJsonHandler glog.Handler = func(ctx context.Context, in *glog.Handler
 func main() {
 	g.Log().SetHandlers(LoggingJsonHandler)
 	ctx := context.TODO()
+	g.Log().SetCtxKeys("Trace-Id", "Span-Id", "Test")
+	ctx = context.WithValue(ctx, "Trace-Id", "1234567890")
+	ctx = context.WithValue(ctx, "Span-Id", "abcdefg")
 	g.Log().Debug(ctx, "Debugging...")
 	g.Log().Warning(ctx, "It is warning info")
 	g.Log().Error(ctx, "Error occurs, please have a check")
